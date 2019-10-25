8b989060d097b3e1af2d23ca471c1c270b42d4af
Rocco Palladino <rpalladino@gmail.com>
2019-10-24 22:13:17 -0500

    Terraform config for root s3 bucket

---
+++ tf/main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+    profile = "default"
+    region  = "us-west-2"
+}
+
+variable "root_domain_name" {
+    default = "roccopalladino.com"
+}
+
+resource "aws_s3_bucket" "root" {
+    bucket = "${var.root_domain_name}"
+    region = "us-west-2"
+    acl    = "public-read"
+
+    website {
+        index_document = "index.html"
+    }
+
+    logging {
+        target_bucket = "logs.${var.root_domain_name}"
+        target_prefix = "root/"
+    }
+}
+
+resource "aws_s3_bucket_policy" "root" {
+    bucket = "${var.root_domain_name}"
+    policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::${var.root_domain_name}/*"
+        }
+    ]
+}
+POLICY
+}
